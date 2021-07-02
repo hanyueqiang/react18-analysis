36bfe0fc35dc3d7712775a7f2fcd1afddea2ed4a
hanyueqiang <webyueqiang@163.com>
2021-07-02 18:42:55 +0800

    :tada: init qReact

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+
+// import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import ReactDom from './qreact/react-dom'
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const jsx = (
+  <div>
+    <h1>hello world</h1>
+    <a href="http://www.baidu.com">ddddddd</a>
+  </div>
+)
+
+ReactDom.render(jsx, document.getElementById("root"))
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
